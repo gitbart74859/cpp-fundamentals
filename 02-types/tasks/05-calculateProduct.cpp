3161df5f3701fa8e3a64056fa925b9156271db0c
Bartlomiej P <gitswist@gmail.com>
2021-06-13 15:42:55 +0200

    Product calculated wiith shared pointers

--- 02-types/tasks/05-calculateProduct.cpp
+++ 02-types/tasks/05-calculateProduct.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
-
+#include <memory>
 // TODO: Implement calculateProduct()
 // It should take 2 int values and return their product as a shared_ptr.
 // Additionally, check how many owners are there.
+std::shared_ptr<int> calculateProduct(int first, int second) {
+    std::shared_ptr<int> product = std::make_shared<int>(first * second);
+
+    return product;
+}
 
 int main() {
     auto number = calculateProduct(10, 20);
